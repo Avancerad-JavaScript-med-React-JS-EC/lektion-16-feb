3456bc394f8cba1d5407e132c4ad34cd9147c689
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-02-16 10:48:13 +0100

    Updates

--- react-context-example/src/App.js
+++ react-context-example/src/App.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 import DisplayUsername from './components/DisplayUsername';
 import ChangeUsername from './components/ChangeUsername';
 
-//Skapar upp vårt context, kan liknas med createStore i redux
+//Skapar upp våra context, kan liknas med createStore i redux
 const UserContext = createContext(undefined);
 const UpdateContext = createContext(undefined);
 
@@ -15,6 +15,8 @@ function App() {
       { /* Gör vårt context tillgängligt för komponenterna som ligger i och sätter ett värde */ }
       { /* Detta värde är det värde vi vill kunna nå i andra komponenter */ }
       <UserContext.Provider value={ username }>
+      { /* Gör vårt context tillgängligt för komponenterna som ligger i och skickar med en set-metod */ }
+      { /* Detta gör att vi kan uppdatera vårt state som i sin tur uppdaterar värdet i UserContext */ }
         <UpdateContext.Provider value={ setUsername }>
           <DisplayUsername />
           <ChangeUsername />
@@ -24,4 +26,4 @@ function App() {
   );
 }
 
-export { App, UserContext };
+export { App, UserContext, UpdateContext };
--- react-context-example/src/components/ChangeUsername.js
+++ react-context-example/src/components/ChangeUsername.js
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from 'react';
-import { UserContext } from '../App';
+import { UserContext, UpdateContext } from '../App';
 
 function ChangeUsername() {
     const user = useContext(UserContext);
+    const update = useContext(UpdateContext);
     const [username, setUsername] = useState(user);
 
     return (
         <section>
             <h2>Byt användarnamn</h2>
             <input type="text" value={ username } onChange={ (event) => setUsername(event.target.value)} />
-            <button>Ändra</button>
+            <button onClick={ () => update(username) }>Ändra</button>
         </section>
     )
 }
